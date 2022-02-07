10b167fa7839afbf4c066873cb74e06568bc99d7
Rohit Gulam <rohitsgulam@gmail.com>
2022-02-07 13:33:00 +0300

    Added waitlist form

--- index.php
+++ index.php
@@ -22,5 +22,21 @@
         <h1 class="tag"> <span class="green">qr</span>menu</h1>
     </div>
     <h2 class="tagline">Digital Menus for Restaurants</h2>
+
+    <div id="mc_embed_signup">
+        <form action="https://itsqrmenu.us14.list-manage.com/subscribe/post?u=30cd9fb5da8041d6abb557bd0&amp;id=4cd683a4a1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+	            <label for="mce-EMAIL">Join our waitlist!</label>
+	            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your Best Email Address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                    <input type="text" name="b_30cd9fb5da8041d6abb557bd0_4cd683a4a1" tabindex="-1" value="">
+                    </div>
+                <div class="clear foot">
+                    <input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="button">
+                </div>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
