f82449cf0256493fca7675c0387a57b4d3073fe9
Rohit Gulam <rohitsgulam@gmail.com>
2022-03-04 10:47:45 +0300

    added categori\y viewing

--- public/main.php
+++ public/main.php
@@ -3,22 +3,28 @@
 ?>
     <?php if(isset($_GET["rest_id"])) : ?>
 
-            <div class="menu-preview">
+        <div class="menu-preview">
                 
                 <div class="banner-area">
                     <div class="img"></div>
                     <h1 class="restaurant-name"><?php echo $userDetails['rest_name'] ?></h1>
                 </div>
+
+                <?php foreach($categories as $category) {?>
+                    <h5 class="category-heading">
+                        <?php echo $category['food_category'] ?>
+                    </h5>
+                    <?php $foods = fetchItemsViaCategories($pdo, $_SESSION['userid'], $category['food_category']); ?>
                     <?php foreach ($foods as $food) { ?>
-                            <div class="menu-area">
-                                <div class="name-price">
-                                    <h3 class="menu-food-title"><?php echo $food['food_name'] ?></h3>
-                                    <p class="menu-food-price">Tsh <?php echo $food['food_price'] ?></p>
-                                </div>
-                                <p class="menu-food-desc"><?php echo $food['food_desc'] ?></p>
+                        <div class="menu-area">
+                            <div class="name-price">
+                                <h3 class="menu-food-title"><?php echo $food['food_name'] ?></h3>
+                                <p class="menu-food-price">Tsh <?php echo $food['food_price'] ?></p>
                             </div>
-            
+                            <p class="menu-food-desc"><?php echo $food['food_desc'] ?></p>
+                        </div>
                     <?php }?>
+                <?php } ?>
 
             </div>
                 </div>
