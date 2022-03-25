0663e5b226401eb33f00e9c52ef4725391d11e11
Rohit Gulam <rohitsgulam@gmail.com>
2022-03-25 13:36:54 +0300

    added canonical url

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="assets/qr-code-1.svg" type="image/x-icon">
+    <link rel="canonical" href="https://itsqrmenu.com" />
     <title>QR Menu - Free Digital Menus For Restaurants</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,8 +14,7 @@
     <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,600;1,100;1,200;1,300;1,400;1,600&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,700;1,100;1,200&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
-<!-- MAilChip -->
-    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7_dtp.css" rel="stylesheet" type="text/css">
+
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-PW7PD96PM5"></script>
@@ -29,26 +29,6 @@
 
 </head>
 <body>
-    <div class="logo">
-        <img src="assets/qr-code-1.svg" alt="QR Menu logo">
-        <h1 class="tag"> <span class="green">qr</span>menu</h1>
-    </div>
-    <h2 class="tagline">Digital Menus for Restaurants</h2>
 
-    <div id="mc_embed_signup">
-        <form action="https://itsqrmenu.us14.list-manage.com/subscribe/post?u=30cd9fb5da8041d6abb557bd0&amp;id=4cd683a4a1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-	            <label for="mce-EMAIL">Join our waitlist!</label>
-	            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your Best Email Address" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                    <input type="text" name="b_30cd9fb5da8041d6abb557bd0_4cd683a4a1" tabindex="-1" value="">
-                    </div>
-                <div class="clear foot">
-                    <input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="button">
-                </div>
-            </div>
-        </form>
-    </div>
 </body>
 </html>
