0bddfb761647b803c4ef0427d0efe509b4e76145
Rohit Gulam <rohitsgulam@gmail.com>
2022-05-18 14:42:06 +0300

    added feature container

--- index.php
+++ index.php
@@ -1,94 +1,101 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="device.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="assets/qr-code-1.svg" type="image/x-icon">
-    <link rel="canonical" href="https://itsqrmenu.com" />
-    <meta name="description" content="">
-    <title>QR Menu - Free Digital Menus For Restaurants</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;600&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,600;1,100;1,200;1,300;1,400;1,600&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,700;1,100;1,200&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+<?php require "header.php" ?>
+<div class="hero-desc">
+    <h1>Upgrade your restaurant with a <span class="underline">QR Menu</span></h1>
+    <p>itsqrmenu allows you to create a QR menu for your restaurant that can be accessed by your customers at their fingertips.</p>
 
-    <!-- Icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Icons -->
+    <a href="#" id="main-create-btn" class="btn">Join the waitlist</a>
+    <p class="small">It's free</p>
 
+</div>
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PW7PD96PM5"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-PW7PD96PM5');
-    </script>
-    
-
-</head>
-<body>
-    <nav class="nav">
-        <div>
-            <img class="logo" src="" alt="itsqrmenu logo">
-            <a href="#" class="btn"> Sign Up</a>
-        </div>
-    </nav>
-    <main>
-        <div class="hero-desc">
-            <h1>Upgrade your resturant with a <span class="underline">QR Menu</span></h1>
-            <p>itsqrmenu allows you to create a QR menu for your restaurant that can be accessed by your customers at their fingertips.</p>
-
-            <a href="#" id="main-create-btn" class="btn">Create my QR Menu</a>
-            <p class="small">It's free</p>
-
-        </div>
-        <!-- <div class="features">
-            <h2>The old way of menus</h2>
-            <ul>
-                <li>You print out your menu</li>
-                <li>You need to update it, so you print it again</li>
-                <li>You hand the same menus to different customers everyday</li>
-                <li>Unfortunately, one customer contacted COVID and touched your menu which is then touched by your waiters and other customers</li>
-                <li>Due to this, a lot of your customers get COVID too</li>
-                <li>People here about this and stop coming to you restaurant</li>
-                <li>Sales decrease forcing you to close down your restaurant</li>
-            </ul>
-        </div> -->
-        <div class="features-container">
-            <div class="features">
-            <h2>The new way of menus</h2>
-                <div class="features-grid" >
-                    <div class="feature-item">
-                        <i class="fa-solid fa-mobile-screen"></i>
-                        <h3>1. Create your menu </h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem commodi cumque sapiente iure, assumenda error temporibus nesciunt ipsam. Perferendis, excepturi?</p>
+<div class="device">
+    <div class="screen">
+        <div class="display" >
+            <div class="menu-preview">
+                <div class="banner-area">
+                    <div class="img"></div>
+                    <h1 class="restaurant-name">Sub-Zero Ice Creams</h1>
+                </div>
+                <div class="menu-area">
+                    <div class="menu-food-item">
+                        <div class="name-price">
+                            <h3 class="menu-food-title">Vanilla Ice Cream</h3>
+                            <p class="menu-food-price">Tsh 3000</p>
+                        </div>
+                        <p class="menu-food-desc"> Good ol' vanilla ice cream</p>
                     </div>
-                    <div class="feature-item">
-                        <i class="fa-solid fa-qrcode"></i>
-                        <h3>2. Print your QR codes only once</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem commodi cumque sapiente iure, assumenda error temporibus nesciunt ipsam. Perferendis, excepturi?</p>
+                        <div class="menu-food-item">
+                        <div class="name-price">
+                            <h3 class="menu-food-title">Vanilla Ice Cream</h3>
+                            <p class="menu-food-price">Tsh 3000</p>
+                        </div>
+                        <p class="menu-food-desc"> Good ol' vanilla ice cream</p>
                     </div>
-                    <div class="feature-item">
-                        <i class="fa-solid fa-mobile-screen-button"></i>
-                        <h3>3. Update your menu whenever you want</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem commodi cumque sapiente iure, assumenda error temporibus nesciunt ipsam. Perferendis, excepturi?</p>
+                    <div class="menu-food-item">
+                        <div class="name-price">
+                            <h3 class="menu-food-title">Strawberry Ice Cream</h3>
+                            <p class="menu-food-price">Tsh 3000</p>
+                        </div>
+                        <p class="menu-food-desc">Strawberry ice cream</p>
                     </div>
-                    <div class="feature-item">
-                        <i class="fa-solid fa-virus-covid-slash"></i>
-                        <h3>4. Decrease transmission of COVID</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem commodi cumque sapiente iure, assumenda error temporibus nesciunt ipsam. Perferendis, excepturi?</p>
+                    <div class="menu-food-item">
+                        <div class="name-price">
+                            <h3 class="menu-food-title">Chocolate Ice Cream</h3>
+                            <p class="menu-food-price">Tsh 3000</p>
+                        </div>
+                        <p class="menu-food-desc">Chocolate ice cream</p>
                     </div>
                 </div>
             </div>
         </div>
-    </main>
-</body>
-</html>
+    </div>
+</div>
+
+<div class="features-container">
+    <div class="non-features">
+        <h2>The old way of menus</h2>
+        <ul>
+            <li>You print out your menu</li>
+            <li>You need to update it, so you print it again</li>
+            <li>You hand the same menus to different customers everyday</li>
+            <li>Unfortunately, a customer has COVID and touches your menu which is then touched by your waiters and other customers</li>
+            <li>Due to this, a lot of your customers get COVID too</li>
+            <li>People hear about this and stop coming to you restaurant</li>
+            <li>Sales decrease forcing you to close down your restaurant</li>
+        </ul>
+    </div>
+    <div class="features">
+    <h2>The new way of menus</h2>
+        <div class="features-grid" >
+            <div class="feature-item">
+                <span class="icon">
+                    <i class="fa-solid fa-mobile-screen"></i>
+                </span>
+                <h3>1. Create your menu </h3>
+                <p>Create your menu with you desired foods and drinks</p>
+            </div>
+            <div class="feature-item">
+                <span class="icon">
+                    <i class="fa-solid fa-qrcode"></i>
+                </span>
+                <h3>2. Print your QR codes only once</h3>
+                <p>Print your QR codes only once and stick them to your tables</p>
+            </div>
+            <div class="feature-item">
+                <span class="icon">
+                    <i class="fa-solid fa-mobile-screen-button"></i>
+                </span>
+                <h3>3. Update your menu whenever you want</h3>
+                <p>Update your menu whenever you want without printing your QR codes again. </p>
+            </div>
+            <div class="feature-item">
+                <span class="icon">
+                    <i class="fa-solid fa-virus-covid-slash"></i>
+                </span>
+                <h3>4. Decrease transmission of COVID</h3>
+                <p>Decrease COVID transmission by removing contact with traditional menus</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require "footer.php" ?>
